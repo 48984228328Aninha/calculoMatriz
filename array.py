081d63af65eadec475525704e48898b6cefb05e5
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-04-10 15:23:51 -0300

    adicionar a função de ver o tamanho

--- array.py
+++ array.py
@@ -9,3 +9,11 @@ B = py.array([[10, 11, 12],
 
 C = A @ B
 print(C)
+
+#criar uma matriz 3x5 sem precisar digitar tudo
+a = py.arrange(15)
+a = a.reshape(3,5)
+print(a)
+print(a.shape)
+print(a.size)
+print(len(a))
