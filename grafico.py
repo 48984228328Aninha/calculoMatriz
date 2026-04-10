d93508de9e88fc7268c9abeca2ae4ba2640a8cc1
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-04-10 16:09:59 -0300

    separar em dois gráficos

--- grafico.py
+++ grafico.py
@@ -16,3 +16,24 @@ plt.show()
 print(f'x: =\n {x}')
 print(f'y_sen: =\n {y_sen}')
 print(f'y_cos: =\n {y_cos}')
+
+#caso os dois gráficos estejam separados :)
+x = np.linespace(-np.pi, np.pi, 35)
+y_sen = np.sin(x)
+y_cos = np.cos(x)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+#gráfico 1
+ax1.grid(True)
+ax1.plot(x, y_sen)
+ax1.set(title='Funções trigonométricas', ylabel = 'sen(x)')
+ax1.set_xlim(-np.pi, np.pi)
+
+#gráfico 2
+ax2.grid(True)
+ax2.plot(x , y_cos, color='orange')
+ax2.set(xlabel = 'Ângulo [rad]', ylabel = 'cos(x)')
+ax2.set_xlim(-np.pi, np.pi)
+
+
